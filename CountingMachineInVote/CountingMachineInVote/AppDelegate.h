3b186a8abc5acff67f231943f183f4b5cf510081
iGhibli <iGhibli@163.com>
2017-04-06 22:07:51 +0800

    Empty Project.

---
+++ CountingMachineInVote/CountingMachineInVote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CountingMachineInVote
+//
+//  Created by matengfei on 2017/4/6.
+//  Copyright © 2017年 iGhibli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CountingMachineInVote/CountingMachineInVote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CountingMachineInVote
+//
+//  Created by matengfei on 2017/4/6.
+//  Copyright © 2017年 iGhibli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
