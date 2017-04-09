8e9378959c3d39162e5ffd0c2bccb50d17ac9adc
iGhibli <iGhibli@163.com>
2017-04-09 20:11:45 +0800

    添加Cell

---
+++ CountingMachineInVote/CountingMachineInVote/Views/SoccerLotteryCell.h
@@ -0,0 +1,13 @@
+//
+//  SoccerLotteryCell.h
+//  CountingMachineInVote
+//
+//  Created by matengfei on 2017/4/9.
+//  Copyright © 2017年 iGhibli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SoccerLotteryCell : UITableViewCell
+
+@end
---
+++ CountingMachineInVote/CountingMachineInVote/Views/SoccerLotteryCell.m
@@ -0,0 +1,24 @@
+//
+//  SoccerLotteryCell.m
+//  CountingMachineInVote
+//
+//  Created by matengfei on 2017/4/9.
+//  Copyright © 2017年 iGhibli. All rights reserved.
+//
+
+#import "SoccerLotteryCell.h"
+
+@implementation SoccerLotteryCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
