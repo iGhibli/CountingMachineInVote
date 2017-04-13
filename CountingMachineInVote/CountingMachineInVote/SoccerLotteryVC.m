69defd636ab1bb51ee340534fed5cbaa7e52dcfc
iGhibli <iGhibli@163.com>
2017-04-13 19:02:31 +0800

    成功请求到数据

--- CountingMachineInVote/CountingMachineInVote/SoccerLotteryVC.m
+++ CountingMachineInVote/CountingMachineInVote/SoccerLotteryVC.m
@@ -9,6 +9,7 @@
 #import "SoccerLotteryVC.h"
 #import "SoccerLotteryCell.h"
 #import "SoccerLotteryModel.h"
+#import "AFNetworking.h"
 
 @interface SoccerLotteryVC ()<UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -26,8 +27,48 @@ static NSString *slCellID = @"SLCellID";
     self.tableView.dataSource = self;
     [self.tableView registerNib:[UINib nibWithNibName:@"SoccerLotteryCell" bundle:nil] forCellReuseIdentifier:slCellID];
     self.edgesForExtendedLayout = UIRectEdgeNone;
+    // 加载数据
+    [self loadDatas];
 }
 
+// 加载数据
+- (void)loadDatas {
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [manager GET:kWYCPURL parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"%@",responseObject);
+        NSArray *tempArray = responseObject[@"matchInfo"];
+        
+//        if (tempArray.count < 4) {
+//            [tempArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//                NSDictionary *dict = obj;
+//                [self.images addObject:dict[@"image"]];
+//                [self.titles addObject:dict[@"title"]];
+//                [self.IDs addObject:dict[@"id"]];
+//            }];
+//        }else {
+//            for (int i=0; i<4; i++) {
+//                NSDictionary *dict = tempArray[i];
+//                [self.images addObject:dict[@"image"]];
+//                [self.titles addObject:dict[@"title"]];
+//                [self.IDs addObject:dict[@"id"]];
+//            }
+//        }
+//        [self.tableView reloadData];
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"%@",error);
+//        if (error.code == -1009 || error.code == -1005) {
+//            MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+//            hud.mode = MBProgressHUDModeText;
+//            hud.label.text = NSLocalizedString(@"网络状态不好,请检查你的网络设置", @"HUD message title");
+//            hud.offset = CGPointMake(0.f, 0.f);
+//            [hud hideAnimated:YES afterDelay:1.5f];
+//        }
+    }];
+}
+
+#pragma mark - UITableView
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
